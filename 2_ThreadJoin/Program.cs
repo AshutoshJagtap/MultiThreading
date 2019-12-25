eab8719d6d930d729c55fd7cdd0adfeade6eeff1
Ashutosh Jagtap <ashujagtap333@gmail.com>
2019-12-25 20:15:02 +0530

    Adedd 2_ThreadJoin project

---
+++ 2_ThreadJoin/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace _2_ThreadJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(PrintMessage);
+            t.Start();
+
+            if (t.Join(10))
+            {
+                Console.WriteLine("Work Completed!");
+            }
+            else
+            {
+                Console.WriteLine("Work not Completed!");
+            }
+            Console.WriteLine($"Main thread exits...{Thread.CurrentThread.ManagedThreadId}");
+            Console.Read();
+        }
+
+        static void PrintMessage()
+        {
+            Console.WriteLine($"Printing msg...{Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(100);
+        }
+    }
+}
