29f87e7513843f9661bcca986071bc854b68b674
Ashutosh Jagtap <ashujagtap333@gmail.com>
2019-12-26 14:44:07 +0530

    Added 5_Task projects

---
+++ 5_Task/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _5_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(Greeding);//hot task
+
+            Task.Factory.StartNew(WhatTypeOfThreadAmI, TaskCreationOptions.LongRunning);//compute-base task
+
+            //Passing data to task and returning data from task
+            Task<string> t = new Task<string>(WhereIam, "Task");//cold task
+            t.Start();
+            Console.WriteLine(t.Result);
+
+            Console.Read();
+        }
+
+        static void Greeding()
+        {
+            Console.WriteLine("Hello World !");
+        }
+        static void WhatTypeOfThreadAmI()
+        {
+            Console.WriteLine("I'm a {0} thread",
+            Thread.CurrentThread.IsThreadPoolThread ? "Thread Pool" : "Custom");
+        }
+
+        static string WhereIam(Object location)
+        {
+            return $"I am in {location}";
+        }
+    }
+}
